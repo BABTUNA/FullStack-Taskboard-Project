3a9d19319ed19d5e5c1c7f54dd8854cb84ed7dc0
BABTUNA <99151318+BABTUNA@users.noreply.github.com>
2022-12-01 09:00:00 -0800

    add create and list task routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 const cors = require('cors');
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/health', (request, response) => {
   response.json({ status: 'ok' });
